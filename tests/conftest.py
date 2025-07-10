7afde91020003cebbf882a31ec81642855702f01
Maks747 <valol93@mail.ru>
2025-07-10 21:17:41 +0300

    Обновил тесты

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,15 @@ from selene import browser
 
 @pytest.fixture
 def desktop_browser():
-    browser.config.window_width = 1366
-    browser.config.window_height = 768
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     yield
     browser.quit()
 
 
 @pytest.fixture
 def mobile_browser():
-    browser.config.window_width = 414
+    browser.config.window_width = 416
     browser.config.window_height = 896
     yield
     browser.quit()
@@ -20,16 +20,16 @@ def mobile_browser():
 @pytest.fixture(params=['desktop', 'mobile'])
 def driver(request):
     if request.param == 'desktop':
-        browser.config.window_width = 1366
-        browser.config.window_height = 768
+        browser.config.window_width = 1920
+        browser.config.window_height = 1080
     else:
-        browser.config.window_width = 414
+        browser.config.window_width = 416
         browser.config.window_height = 896
     yield
     browser.quit()
 
 
-@pytest.fixture(params=[(1366, 768), (414, 896)])
-def driver_setup(request):
+@pytest.fixture(params=[(1920, 1080), (416, 896)])
+def driver_config(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
--- tests/test_skip.py
+++ tests/test_skip.py
@@ -3,16 +3,20 @@
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 import pytest
+from selene import browser,have
 
 
-@pytest.fixture()
-def browser():
-    pass
+def test_github_desktop(driver_config):
+    if browser.config.window_height < 1000:
+        pytest.skip("Разрешение экрана для Mobile")
+    browser.open('https://github.com/')
+    browser.element(".HeaderMenu-link--sign-in").click()
+    browser.element("h1").should(have.text("Sign in to GitHub"))
 
-
-def test_github_desktop(browser):
-    pass
-
-
-def test_github_mobile(browser):
-    pass
+def test_github_mobile(driver_config):
+    if browser.config.window_height > 1000:
+        pytest.skip("Разрешение экрана для Desktop")
+    browser.open('https://github.com/')
+    browser.element('[class=Button-content]').click()
+    browser.element('[href="/login"]').click()
+    browser.element("h1").should(have.text("Sign in to GitHub"))
